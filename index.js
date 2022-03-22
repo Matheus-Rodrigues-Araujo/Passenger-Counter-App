e205cdba1abce87f48b6cdd2b076890e1844bde9
Matheus-Rodrigues-Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2022-03-22 14:48:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// gets the element by the id
+let countEl = document.getElementById('count-el')
+// let count_value = Number(countEl.value)
+
+// Variable that will be incremented
+let count = 0;
+
+// Increment the value
+function increment(){
+    count ++
+    countEl.innerText = count
+}
+// Decrement the value
+function decrement(){
+    count--
+    countEl.innerText = count
+}
