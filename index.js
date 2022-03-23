3a01836eb610f825c2c3a2a7b9c8b13013a9cf66
Matheus-Rodrigues-Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2022-03-23 14:55:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // gets the element by the id
 let countEl = document.getElementById('count-el')
+let display = document.getElementById('displayData')
 // let count_value = Number(countEl.value)
 
 // Variable that will be incremented
@@ -16,4 +17,19 @@ function decrement(){
         count--
         countEl.innerText = count
     }
+}
+
+function saveQuantity(){
+    // let display = document.getElementById('displayData')
+    display.style.display = 'block'
+    display.innerText+= ` ${count} - `
+    
+    
+}
+
+function reset(){
+    count = 0
+    countEl.innerText = 0
+    display.innerText = 'Previous entries: '
+    display.style.display = 'none'
 }
